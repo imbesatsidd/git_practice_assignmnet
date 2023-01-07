13a7a96e664dc7a518f04c5a0247c9a1065aca3f
unknown <imbesatsid@gmail.com>
2023-01-08 01:49:55 +0530

    palindrome problem done

---
+++ palindrome.js
@@ -0,0 +1,12 @@
+ let str="naman"
+ let str1=""
+  for(i=N-1;i>=0;i++){
+      str1=str1+str[i]
+      
+      }
+      if(str==str1){
+          console.log("Yes")
+      }
+      else{
+          console.log("No")
+      }
